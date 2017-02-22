d680ae458e6fa4ce66c9a6dbb8cbf77f91f47c64
Mark Luko <mark.luko@gmail.com>
2017-02-22 11:27:56 +0200

    1st challenge: Code cleanup

--- 1/challenge1.py
+++ 1/challenge1.py
@@ -4,20 +4,22 @@ import sys
 # Ceil division
 def ceildiv(a,b): 
     return (a + (b-1))//b;
-    
+
+# Integer to 6bit bytes    
 def int2bytes(x):
     ln = ceildiv(x.bit_length(), 6);
     bytes = [0]*ln;
     for i in range(ln):
         bytes[ln - i - 1] = x & 0x3F;
         x >>= 6;
-        
     return bytes;
-    
+
+# Convert 6bit byte to base64    
 def base64_byte(x):
     b64 = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+\\'
     return b64[x];
 
+# Convert byte array to base64 string
 def baset64(bytes):
     str = '';
     for b in bytes:
