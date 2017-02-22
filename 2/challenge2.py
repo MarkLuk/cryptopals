255e3fe898f2c9cde294809ecb6e706d82c41e88
Mark Luko <mark.luko@gmail.com>
2017-02-22 11:36:50 +0200

    2nd challenge

---
+++ 2/challenge2.py
@@ -0,0 +1,14 @@
+#!python
+import sys
+
+   
+# Main entry point
+def main():
+    # Get 2 hex integers
+    x1 = int(sys.argv[1], 16);
+    x2 = int(sys.argv[2], 16);
+    
+    print(hex(x1^x2))
+    
+if __name__ == "__main__":
+    main()
