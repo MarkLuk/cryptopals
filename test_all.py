6e84e5c6bb9de04cfdc25e1cec37b13e550418d7
Mark Luko <mark.luko@gmail.com>
2017-02-23 15:18:43 +0200

    Regression test

---
+++ test_all.py
@@ -0,0 +1,33 @@
+#!python
+import challenge1 as ch1
+import challenge2 as ch2
+import challenge3 as ch3
+import challenge4 as ch4
+import challenge5 as ch5
+import challenge6 as ch6
+import challenge7 as ch7
+import challenge8 as ch8
+import challenge9 as ch9
+
+def main():
+    print ("Challenge #1")
+    ch1.main()
+    print ("Challenge #2")
+    ch2.main()
+    print ("Challenge #3")
+    ch3.main()
+    print ("Challenge #4")
+    ch4.main()
+    print ("Challenge #5")
+    ch5.main()
+    print ("Challenge #6")
+    ch6.main()
+    print ("Challenge #7")
+    ch7.main()
+    print ("Challenge #8")
+    ch8.main()
+    print ("Challenge #9")
+    ch9.main()
+
+if __name__ == "__main__":
+    main()
