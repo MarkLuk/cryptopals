6e16c8f8320a17ccbeec6de7c36c59ce44fc80d3
Mark Luko <mark.luko@gmail.com>
2017-02-23 09:47:47 +0200

    Moved to Python3 build-in implementation of base64

--- challenge1.py
+++ challenge1.py
@@ -1,36 +1,19 @@
 #!python
 import sys
 import utils
+import base64
 
-# Integer to 6bit bytes    
-def int_6bit_bytes(x):
-    ln = utils.ceildiv(x.bit_length(), 6);
-    bytes = [0]*ln;
-    for i in range(ln):
-        bytes[ln - i - 1] = x & 0x3F;
-        x >>= 6;
-    return bytes;
-
-# Convert 6bit byte to base64    
-def 6bit_base64(x):
-    b64 = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/'
-    return b64[x];
-
-# Convert byte array to base64 string
-def bytes_base64(bytes):
-    str = '';
-    for b in bytes:
-        str += 6bit_base64(b);
-    return str;
     
 # Main entry point
 def main(hexstr):
-    # Get integer for hex-string
-    x = utils.hexstr_int(hexstr);
-    # Convert integer to 6bit bytes
-    bytes = int_6bit_bytes(x);
-    # Convert 6bit bytes to base64
-    print (bytes_base64(bytes));
-    
+    # Convert to bytes
+    bytes = utils.hexstr_bytes(hexstr)
+    # Calculate base64 bytes
+    enc_bytes=base64.b64encode(bytes)
+    # Convert bytes to string
+    str = utils.bytes_string(enc_bytes)
+    print (str)
+
+
 if __name__ == "__main__":
     main('49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d')
