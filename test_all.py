a6494385daea6e15226f5ec277c2f37957179651
Mark Luko <mark.luko@gmail.com>
2017-03-02 15:21:41 +0200

    Update test

--- test_all.py
+++ test_all.py
@@ -1,13 +1,15 @@
 #!python
-import challenge1 as ch1
-import challenge2 as ch2
-import challenge3 as ch3
-import challenge4 as ch4
-import challenge5 as ch5
-import challenge6 as ch6
-import challenge7 as ch7
-import challenge8 as ch8
-import challenge9 as ch9
+import challenge01 as ch1
+import challenge02 as ch2
+import challenge03 as ch3
+import challenge04 as ch4
+import challenge05 as ch5
+import challenge06 as ch6
+import challenge07 as ch7
+import challenge08 as ch8
+import challenge09 as ch9
+import challenge10 as ch10
+import challenge11 as ch11
 
 def main():
     print ("Challenge #1")
@@ -28,6 +30,10 @@ def main():
     ch8.main()
     print ("Challenge #9")
     ch9.main()
+    print ("Challenge #10")
+    ch10.main()
+    print ("Challenge #11")
+    ch11.main()
 
 if __name__ == "__main__":
     main()
