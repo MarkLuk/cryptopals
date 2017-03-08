0572abda17b3c85832b300590a1ff738c8a97073
Mark Luko <mark.luko@gmail.com>
2017-03-08 11:52:58 +0200

    Moved some SHA1 functions to utils

--- SHA1.py
+++ SHA1.py
@@ -2,16 +2,6 @@
 from utils import *
 
 
-# Rotate left: 0b1001 --> 0b0011
-rol = lambda val, r_bits, max_bits: \
-    (val << r_bits%max_bits) & (2**max_bits-1) | \
-    ((val & (2**max_bits-1)) >> (max_bits-(r_bits%max_bits)))
- 
-# Rotate right: 0b1001 --> 0b1100
-ror = lambda val, r_bits, max_bits: \
-    ((val & (2**max_bits-1)) >> r_bits%max_bits) | \
-    (val << (max_bits-(r_bits%max_bits)) & (2**max_bits-1))
-
 def int32(x):
     return x&0xFFFFFFFF
     
--- utils.py
+++ utils.py
@@ -84,3 +84,12 @@ def bit_set(n, i, val):
     else:
         return n | (1<<i)
 
+# Rotate left: 0b1001 --> 0b0011
+rol = lambda val, r_bits, max_bits: \
+    (val << r_bits%max_bits) & (2**max_bits-1) | \
+    ((val & (2**max_bits-1)) >> (max_bits-(r_bits%max_bits)))
+ 
+# Rotate right: 0b1001 --> 0b1100
+ror = lambda val, r_bits, max_bits: \
+    ((val & (2**max_bits-1)) >> r_bits%max_bits) | \
+    (val << (max_bits-(r_bits%max_bits)) & (2**max_bits-1))
