adf9b0da8215169da87ac12a0a8def231aa9ae36
Mark Luko <mark.luko@gmail.com>
2017-03-21 14:11:36 +0200

    Thread syncronization added

--- challenge33.py
+++ challenge33.py
@@ -9,30 +9,41 @@ g = 2
 alice_id = 0
 bob_id = 1
 public_keys = [0, 0];
+public_keys_ready = [threading.Condition(), threading.Condition()];
 thread_names = ['Alice', 'Bob']
 
-def thread_main(my_id, party_id,public_keys):
+def thread_main(my_id, party_id,public_keys, public_keys_ready):
     # Generate public key
     my_a = bytes_int(random_bytes(16))
     my_public = modular_pow(g, my_a, p)
+
     # Publish public key
+    public_keys_ready[my_id].acquire();
     public_keys[my_id] = my_public
+    public_keys_ready[my_id].notify();
+    public_keys_ready[my_id].release();
+
     # Wait for other public key
+    public_keys_ready[party_id].acquire();
     while (public_keys[party_id] == 0):
-        pass
+        public_keys_ready[party_id].wait();
+    pub_key = public_keys[party_id];
+    public_keys_ready[party_id].release();
+
     # Generate session key
-    session_key = modular_pow(public_keys[party_id], my_a, p);
+    session_key = modular_pow(pub_key, my_a, p);
     print ('Thread', thread_names[my_id], 'session key: ', hex(session_key))
+
 
 def main():
     global alice_id
     global bob_id
 
     # Start Alice thread
-    alice = threading.Thread(target=thread_main, args=(alice_id, bob_id, public_keys))
+    alice = threading.Thread(target=thread_main, args=(alice_id, bob_id, public_keys, public_keys_ready))
     alice.start()
     # Start Bob thread
-    bob = threading.Thread(target=thread_main, args=(bob_id, alice_id, public_keys))
+    bob = threading.Thread(target=thread_main, args=(bob_id, alice_id, public_keys, public_keys_ready))
     bob.start()
 
     # Wait for threads to finish
